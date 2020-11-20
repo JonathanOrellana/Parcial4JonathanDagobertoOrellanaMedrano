aa9cb4d85057cc6a5abcbd90a1c18b63de17df6e
JonathanOrellana <70900904+JonathanOrellana@users.noreply.github.com>
2020-11-20 14:14:00 -0600

    ejercicios

---
+++ Ejercicio2ParcialLambda/Ejercicio2ParcialLambda/Delegado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2ParcialLambda
+{
+    delegate double total(double a);
+    class Delegado
+    {
+        public double desc(double a)
+        {
+            double resultado = 0;
+            if (a > 10000 & a < 20000)
+            {
+                resultado = a * 0.10;
+
+            }
+            if (a > 20001 & a < 50000)
+            {
+                    resultado = a * 0.30;
+
+            }
+            if (a > 50000)
+            {
+                resultado = a * 0.50;
+
+            }
+            return resultado ;
+        }
+        
+    }
+}
